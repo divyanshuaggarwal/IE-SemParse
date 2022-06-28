e1dd3740d3a71620e89298421e29712b9eb1ae51
divyanshuaggarwal <divyanshuggrwl@gmail.com>
2022-06-29 00:41:01 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#bin/bash
+
+pip install -r requirements.txt
+
+echo "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" > ~/.huggingface
+
+apt-get update && apt-get upgrade -y
+
+apt-get install tmux git-lfs htop
+
+git lfs install
+git clone https://huggingface.co/datasets/Divyanshu/Indic-SemParse
+
+tmux
+
+python autostop.py
+
+tmux detach
